397f65cbc9cccf3a196fce9cef1ba7c4dae1da86
Seolang <dntmdqls0912@gmail.com>
2022-09-16 14:51:46 +0900

    useEffect and deps set

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -4,20 +4,48 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => {
     setCounter((prev) => prev + 1);
-  }
-  console.log("i run all the time");
+  };
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  // useEffect run only the first render time.
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
+
+  // search run only when keyword changes
+  // watching states 'keyword'
+  useEffect(() => {
+    if (keyword != "" && keyword.length > 5) {
+      console.log("I run when 'keyword' changes");
+    }
+  }, [keyword]);
 
   useEffect(() => {
-    console.log("Call the API");
-  }, []);
-  // useEffect run only the first render time.
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+
+  // Deps could add multiple states
+  useEffect(() => {
+    console.log("I run when 'counter and keyword' changes");
+  }, [counter, keyword]);
+
 
   return (
     <div className="App">
+      <input 
+        value={keyword} 
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here..." 
+      />
       <h1 className={styles.title}>{counter}</h1>
-      <Button text={"Continue!"}/>
+       {/*<Button text={"Continue!"} />*/}
       <button onClick={onClick}>Click me</button>
     </div>
   );
