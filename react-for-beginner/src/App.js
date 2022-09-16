54c8e9f22299e877ec3797b745e12359ab8e19e3
Seolang <dntmdqls0912@gmail.com>
2022-09-16 13:33:58 +0900

    React App CSS class and Props

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => {
+    setCounter((prev) => prev + 1);
+  }
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("Call the API");
+  }, []);
+  // useEffect run only the first render time.
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className={styles.title}>{counter}</h1>
+      <Button text={"Continue!"}/>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
