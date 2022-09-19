ff4c93f1db33450edac6baab7a59ba1520bbe15e
Seolang <dntmdqls0912@gmail.com>
2022-09-19 16:03:06 +0900

    Movie App part two

--- react-for-beginner/src/index.js
+++ react-for-beginner/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import CoinTracker from './CoinTracker';
+import MovieApp from './MovieApp';
 //import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CoinTracker />
+    <MovieApp />
   </React.StrictMode>
 );
