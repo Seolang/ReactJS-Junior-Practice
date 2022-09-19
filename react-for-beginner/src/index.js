cbc16e2e89da40d766fb9d1329ab19815582420c
Seolang <dntmdqls0912@gmail.com>
2022-09-19 15:03:44 +0900

    build coinTracker

--- react-for-beginner/src/index.js
+++ react-for-beginner/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import CoinTracker from './CoinTracker';
 //import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CoinTracker />
   </React.StrictMode>
 );
